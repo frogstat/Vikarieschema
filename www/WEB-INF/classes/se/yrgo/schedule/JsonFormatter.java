c500e42fdb5a921f07999d4606974f9be5a38f87
frogstat <frogstatftw@gmail.com>
2026-01-29 11:46:00 +0100

    Add empty formatters for json and xml

---
+++ www/WEB-INF/classes/se/yrgo/schedule/JsonFormatter.java
@@ -0,0 +1,12 @@
+package se.yrgo.schedule;
+
+import java.util.List;
+
+public class JsonFormatter implements Formatter{
+
+    @Override
+    public String format(List<Assignment> assignments) {
+        return "";
+    }
+
+}
---
+++ www/WEB-INF/classes/se/yrgo/schedule/XmlFormatter.java
@@ -0,0 +1,12 @@
+package se.yrgo.schedule;
+
+import java.util.List;
+
+public class XmlFormatter implements Formatter{
+
+    @Override
+    public String format(List<Assignment> assignments) {
+        return "";
+    }
+    
+}
