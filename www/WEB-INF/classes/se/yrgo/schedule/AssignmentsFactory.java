1095310e465ce13d32b56997b7874cf0337f1f73
frogstat <frogstatftw@gmail.com>
2026-01-29 11:31:27 +0100

    Initial commit

---
+++ www/WEB-INF/classes/se/yrgo/schedule/AssignmentsFactory.java
@@ -0,0 +1,11 @@
+package se.yrgo.schedule;
+
+public class AssignmentsFactory {
+    private AssignmentsFactory() {
+    }
+
+    public static Assignments getAssignments() {
+        return new DatabaseAssignments();
+    }
+
+}
