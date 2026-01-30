739add5c13956bb47b9b63ad75f7f58268025310
frogstat <frogstatftw@gmail.com>
2026-01-30 11:48:13 +0100

    finish JSONFormatter

--- compile_servlet_and_start_winstone.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-PATHSEP=":"
-if [[ $OS == "Windows_NT" ]] || [[ $OSTYPE == "cygwin" ]]
-then
-    PATHSEP=";"
-fi
-
-javac -cp "./www/WEB-INF/classes/${PATHSEP}winstone.jar" www/WEB-INF/classes/se/yrgo/schedule/*.java && java -jar winstone.jar --webroot=www
--- www/WEB-INF/classes/se/yrgo/schedule/JsonFormatter.java
+++ www/WEB-INF/classes/se/yrgo/schedule/JsonFormatter.java
@@ -1,38 +1,33 @@
 package se.yrgo.schedule;
 
+import org.json.*;
+
 import java.util.List;
 
-public class JsonFormatter implements Formatter{
+public class JsonFormatter implements Formatter {
 
     @Override
     public String format(List<Assignment> assignments) {
 
+        try {
+            JSONObject jo = new JSONObject();
+            JSONArray schedule = new JSONArray();
 
+            for (Assignment assignment : assignments) {
+                JSONObject jsonJob = new JSONObject();
+                jsonJob.put("school_name", assignment.school().getName());
+                jsonJob.put("school_address", assignment.school().getAddress());
+                jsonJob.put("date", assignment.date());
+                jsonJob.put("substitute", assignment.substitute().getName());
+                schedule.put(jsonJob);
+            }
 
-        return """
-                {
-                    "glossary": {
-                        "title": "example glossary",
-                		"GlossDiv": {
-                            "title": "S",
-                			"GlossList": {
-                                "GlossEntry": {
-                                    "ID": "SGML",
-                					"SortAs": "SGML",
-                					"GlossTerm": "Standard Generalized Markup Language",
-                					"Acronym": "SGML",
-                					"Abbrev": "ISO 8879:1986",
-                					"GlossDef": {
-                                        "para": "A meta-markup language, used to create markup languages such as DocBook.",
-                						"GlossSeeAlso": ["GML", "XML"]
-                                    },
-                					"GlossSee": "markup"
-                                }
-                            }
-                        }
-                    }
-                }
-                """;
+            jo.put("schedule", schedule);
+            return jo.toString();
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
