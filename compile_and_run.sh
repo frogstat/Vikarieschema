f2eca5abf712e30722fa341ef0993d9db08f951e
frogstat <frogstatftw@gmail.com>
2026-01-30 14:43:46 +0100

    update run script

--- compile_and_run.sh
+++ compile_and_run.sh
@@ -1,1 +1,6 @@
+echo "sites:"
+echo "  http://localhost:8080/v1?format=json"
+echo "  http://localhost:8080/v1?format=xml"
+echo ""
+
 javac -cp .:www/WEB-INF/classes/:winstone.jar:www/WEB-INF/lib/android-json-6.0.1.10.jar www/WEB-INF/classes/se/yrgo/schedule/*/*.java && java -jar winstone.jar --webroot=www
