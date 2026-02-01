400510dc2eb5fbe5b3bab3816cdf4f52b26dfcf7
frogstat <frogstatftw@gmail.com>
2026-02-01 18:49:11 +0100

    add status code test

---
+++ test_head.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+fail() {
+    echo $1
+    exit 1
+}
+
+check_status(){
+    if [ $1 -eq "000" ]; then
+        fail "Could not connect. Is the server up?"
+    elif [ ! $1 -eq $2 ]; then
+        fail "Error. Got $1 but expected $2"
+    fi
+}
+
+
+echo "Testing headers..."
+echo ""
+echo "Testing valid URL. Should return 200"
+sleep 1
+my_status=$(curl -o /dev/null --silent --head --write-out '%{http_code}\n' "http://localhost:8080/v1?format=json&substitute_id=1")
+check_status $my_status "200"
+echo $my_status
+
+sleep 1
+echo "Testing valid URL, but invalid substitute_id. Should return 404."
+sleep 1
+my_status=$(curl -o /dev/null --silent --head --write-out '%{http_code}\n' "http://localhost:8080/v1?format=json&substitute_id=122")
+check_status $my_status "404"
+echo $my_status
+
+sleep 1
+echo "Testing invalid format. Should return 400."
+sleep 1
+my_status=$(curl -o /dev/null --silent --head --write-out '%{http_code}\n' "http://localhost:8080/v1?format=idunno")
+check_status $my_status "400"
+echo $my_status
+exit 0
